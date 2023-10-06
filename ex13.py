f5543b5ab79a3ffb7e78b380b8b8c224a399379f
66056073 <66056073@kmitl.ac.th>
2023-10-06 22:07:29 +0700

    ส่ง Assignment calculateSum() calculateProduct() average() median()

--- ex13.py
+++ ex13.py
@@ -1,31 +1,43 @@
-def calculateSum(list_data):
-    if list_data.__len__() == 0:
+def calculateSum(numbers):
+    if numbers.__len__() == 0:
         return 0
     else:
-        total = 0
-        for i in list_data:
-            total = total + i
-        return total
+        result = 0
+        for i in numbers:
+            result = result + i
+        return result
 
 
-def calculateProduct(list_data):
-    if list_data.__len__() == 0:
+def calculateProduct(numbers):
+    if numbers.__len__() == 0:
         return 1
     else:
         product = 1
-        for i in list_data:
+        for i in numbers:
             product = product * i
         return product
 
 
-def average(list_data):
-    if list_data.__len__() == 0:
-        return 0
+def average(numbers):
+    if numbers.__len__() == 0:
+        return None
     else:
-        total = 0
-        for i in list_data:
-            total = total + i
-        return total / list_data.__len__()
+        result = 0
+        for i in numbers:
+            result = result + i
+        return result / numbers.__len__()
+
+
+def median(numbers):
+    if numbers.__len__() == 0:
+        return None
+    else:
+        numbers.sort()
+        middleIndex = numbers.__len__() // 2
+        if numbers.__len__() % 2 == 0:
+            return (numbers[middleIndex] + numbers[middleIndex - 1]) / 2
+        else:
+            return numbers[middleIndex]
 
 
 if __name__ == '__main__':
@@ -35,11 +47,15 @@ if __name__ == '__main__':
     print(calculateProduct([]) == 1)
     print(calculateProduct([2, 4, 6, 8, 10]) == 3840)
     print('\nCalculate Average Check')
+    print(average([]) is None)
     print(average([1, 2, 3]) == 2)
     print(average([1, 2, 3, 1, 2, 3, 1, 2, 3]) == 2)
     print(average([12, 20, 37]) == 23)
     print(average([0, 0, 0, 0, 0]) == 0)
-
-#TEST4
+    print('\nCalculate Median Check')
+    print(median([]) is None)
+    print(median([1, 2, 3]) == 2)
+    print(median([3, 7, 10, 4, 1, 9, 6, 5, 2, 8]) == 5.5)
+    print(median([3, 7, 10, 4, 1, 9, 6, 2, 8]) == 6)
 
 # %%
