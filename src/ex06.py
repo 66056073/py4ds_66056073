affc5d33fd669e0e6786ba956eb9d7f48e51b6a2
66056073 <66056073@kmitl.ac.th>
2023-10-13 12:56:24 +0700

    Fix ordinal_suffix()

--- src/ex06.py
+++ src/ex06.py
@@ -3,7 +3,7 @@ Execise 6
 """
 
 
-def ordinal_suffix(param):
+def ordinal_suffix(num):
     """
     Return the ordinal suffix for a given number.
 
@@ -13,5 +13,14 @@ def ordinal_suffix(param):
     Returns:
         str: The ordinal suffix corresponding to the given number.
     """
-    # TODO : complete this
-    pass
+    # FIX : complete this
+    if num.__str__()[-2:] == '11' or num.__str__()[-2:] == '12' or num.__str__()[-2:] == '13':
+        return num.__str__() + 'th'
+    elif num.__str__()[-1:] == '1':
+        return num.__str__() + 'st'
+    elif num.__str__()[-1:] == '2':
+        return num.__str__() + 'nd'
+    elif num.__str__()[-1:] == '3':
+        return num.__str__() + 'rd'
+    else:
+        return num.__str__() + 'th'
