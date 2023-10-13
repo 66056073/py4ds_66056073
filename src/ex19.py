cf7259ce8902129a1d79ab2442bd044a35edb63e
66056073 <66056073@kmitl.ac.th>
2023-10-13 20:54:58 +0700

    Fix gen_password() ex19

--- src/ex19.py
+++ src/ex19.py
@@ -3,14 +3,25 @@ Exercise 19 : Password Generator
 """
 import random
 
-LOWER_LETTERS = 'abcdefghijklmnopqrstuvwxyz'    # 26
-UPPER_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'    # 26
-NUMBERS = '1234567890'                          # 10
-SPECIAL = '~!@#$%^&*()_+'                       # 13
+LOWER_LETTERS = 'abcdefghijklmnopqrstuvwxyz'  # 26
+UPPER_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'  # 26
+NUMBERS = '1234567890'  # 10
+SPECIAL = '~!@#$%^&*()_+'  # 13
 
 ALL_CHARS = LOWER_LETTERS + UPPER_LETTERS + NUMBERS + SPECIAL
 
 
-def gen_password(param):
+def gen_password(char_num):
     # TODO : complete this
-    pass
+    password = []
+    if char_num < 12:
+        char_num = 12
+    password.append((random.choice(LOWER_LETTERS)))
+    password.append((random.choice(UPPER_LETTERS)))
+    password.append((random.choice(NUMBERS)))
+    password.append((random.choice(SPECIAL)))
+    for i in range(char_num-4):
+        password.append(random.choice(ALL_CHARS))
+    random.shuffle(password)
+    return password
+
