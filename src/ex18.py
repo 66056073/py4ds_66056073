34871faea23acd157c336ae5d5389d1e68f62f88
66056073 <66056073@kmitl.ac.th>
2023-10-13 19:59:11 +0700

    Fix get_cost_of_coffee() ex18

--- src/ex18.py
+++ src/ex18.py
@@ -3,5 +3,11 @@ Exercise 18 : Buy 8 get 1 free
 """
 
 
-def get_cost_of_coffee(param, param1):
-    pass
+def get_cost_of_coffee(amount, price):
+    discount_time = amount // 9
+    return (amount - discount_time) * price
+
+
+def get_cost_of_coffee_2(amount, price):
+    discount_time = amount // 9
+    return (amount - discount_time) * price
